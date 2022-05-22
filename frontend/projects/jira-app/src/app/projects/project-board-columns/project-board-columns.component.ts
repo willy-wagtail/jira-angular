e7f97eb5178b103f0329a1be9880c68c6687cb7d
William Cheung <william.cheung.c@gmail.com>
2022-05-22 23:47:30 +0100

    added page-not-found added active sidebar link styling added routing added projects module added project board columns added project board header added project sidebar added application header

---
+++ frontend/projects/jira-app/src/app/projects/project-board-columns/project-board-columns.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-board-columns',
+  templateUrl: './project-board-columns.component.html',
+  styleUrls: ['./project-board-columns.component.scss'],
+})
+export class ProjectBoardColumnsComponent {
+  constructor() {}
+}
---
+++ frontend/projects/jira-app/src/app/projects/project-board/project-board.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-board',
+  templateUrl: './project-board.component.html',
+  styleUrls: ['./project-board.component.scss'],
+})
+export class ProjectBoardComponent {
+  constructor() {}
+}
